1e0a99e5f7cec4bd03cf4edbef21abfc86c97d1c
ludivine Pugellier <ludivine.pugellier@outlook.fr>
2024-12-04 15:26:00 +0100

    création d'un scriptshell pour automatisée la création de département

---
+++ scriptingshell.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+mkdir var
+mkdir var/www
+
+#Premier dépatrement
+mkdir -p /var/www/dep-info
+chmod 770 /var/www/dep-info
+cd /var/www/dep-info
+touch index.html
+chmod 770 index.html
+echo "<h1>Bienvenue dans le département Informatique</h1>" > index.html
+cd ..
+
+#deuxième département
+mkdir -p /var/www/dep-manage
+chmod 770 /var/www/dep-manage
+cd /var/www/dep-manage
+touch index.html
+chmod 770 index.html
+echo "<h1>Bienvenue dans le département management</h1>" > index.html
+cd ..
+
+#troisième département
+mkdir -p /var/www/dep-mark
+chmod 770 /var/www/dep-mark
+cd /var/www/dep-mark
+touch index.html 
+chmod 770 index.html
+echo "<h1>Bienvenue dans le département marketing</h1>" > index.html
+cd ..
