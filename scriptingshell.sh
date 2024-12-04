ab33ffc2a339ba8bc123ed873d366584adc5c30f
ludivine Pugellier <ludivine.pugellier@outlook.fr>
2024-12-04 16:29:32 +0100

    modification code automatisation sur scriptingshell

--- scriptingshell.sh
+++ scriptingshell.sh
@@ -1,25 +1,26 @@
 #!/bin/bash
 mkdir var
 mkdir var/www
+chmod 777 /var/www
 
 #Premier dépatrement
 mkdir -p /var/www/dep-info
-chmod 770 /var/www/dep-info
+chmod 777 /var/www/dep-info
 cd /var/www/dep-info
 touch index.html
 chmod 770 index.html
 echo "<h1>Bienvenue dans le département Informatique</h1>" > index.html
-python3 -m http.server 3001
+python3 -m http.server 3001 &
 cd ..
 
 #deuxième département
 mkdir -p /var/www/dep-manage
-chmod 770 /var/www/dep-manage
+chmod 777 /var/www/dep-manage
 cd /var/www/dep-manage
 touch index.html
-chmod 770 index.html
+chmod 777 index.html
 echo "<h1>Bienvenue dans le département management</h1>" > index.html
-python3 -m http.server 3002
+python3 -m http.server 3002 &
 cd ..
 
 #troisième département
@@ -29,5 +30,8 @@ cd /var/www/dep-mark
 touch index.html 
 chmod 770 index.html
 echo "<h1>Bienvenue dans le département marketing</h1>" > index.html
-python3 -m http.server 3003
+python3 -m http.server 3003 &
 cd ..
+
+# Redémarrer Nginx
+systemctl restart nginx
